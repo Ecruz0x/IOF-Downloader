8b9569c7f236090ae09e58bb6027a243bc13012b
A.Naciri <abdelhaqnaciri0@gmail.com>
2025-04-05 01:50:57 +0000

    First Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from requests_html import HTML, HTMLSession
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+
+indexofurl = input("Enter IOF URL : ")
+filetype = input("Enter the filetype : ")
+dirn = input("Enter SaveDIR : ")
+
+try:
+    os.mkdir(f"./{dirn}")
+except FileExistsError:
+    pass
+
+def urlsParsing(indexofurl, filetype):
+    session = HTMLSession()
+    r = session.get(indexofurl)
+    rhtml = r.html.html
+    soup = bs(rhtml, 'html.parser')
+    urls = []
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if url[-len(filetype):] == filetype:
+            urls.append(url)
+    return urls
+
+def FilesDL(urls):
+    for i in urls:
+        response = requests.get(indexofurl+i)
+        if response.status_code == 200 :
+            print(f"Downloaded file : {i}")
+            with open(f"./pdfs2/{i}", "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        else:
+            print(f"Cannot download file : {i}")
+        
+urls = urlsParsing(indexofurl, filetype)
+FilesDL(urls)
+
